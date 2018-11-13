4ebd63c2a81a5df34f6044eb4a0d9fdda425236f
Ammon Carson <pianoace1315@gmail.com>
2018-11-13 16:32:20 -0700

    Document Edit Works (Lesson 8)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -44,7 +45,7 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
     ContactEditComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule
   ],
   providers: [WindRefService],
   bootstrap: [AppComponent]
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from '../document.model';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { DocumentsService } from '../documents.service';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'cms-document-edit',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(private documentService: DocumentsService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        const id = params.id;
+        if (!id) {
+          return;
+        }
+        this.originalDocument = this.documentService.getDocument(id);
+        if (!this.originalDocument) {
+          return;
+        }
+        this.editMode = true;
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      }
+    )
   }
 
+  onSubmit(form: NgForm){
+    let newId = this.documentService.getMaxId();
+    newId = newId++;
+    let values = form.value;
+    let newDocument = new Document(newId.toString(), values.name, values.description, values.documentUrl, values.children);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+        this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['/documents']);
+    console.log(form.value);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
 }
